f6a0bacc97b91a37ca9db56f0d33de704d83ff1e
Benjamin Shen <bfs45@cornell.edu>
2019-03-28 14:17:09 -0400

    button style

--- test.js
+++ test.js
@@ -1,34 +1,45 @@
 var prompts = [
 {
-    prompt: "question1?",
-    choices: ["this","this2","that","that2"],
-    weight: [[0,0],[0,0],[0,0],[0,0]],
+    prompt: "What is your favorite warmup?",
+    weight: 0.3,
+    choices: ["9 vowels","octamolly-dominorky","falsetto","pretentious yawning"],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
 },
 {
-    prompt: "question2?",
-    choices: ["this","this2","that","that2"],
-    weight: [[0,0],[0,0],[0,0],[0,0]],
+    prompt: "How many Cornell songs do you know by heart?",
+    weight: 0.3,
+    choices: ["none","one","two","more"],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
 },
 {
-    prompt: "question3?",
-    choices: ["this","this2","that","that2"],
-    weight: [[0,0],[0,0],[0,0],[0,0]],
+    prompt: "You are in rehearsal and you think we are singing off key. You",
+    weight: 0.1,
+    choices: ["do nothing.","doubt yourself.","tell a neighbor.","correct Robert."],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
 },
 {
-    prompt: "question4?",
-    choices: ["this","this2","that","that2"],
-    weight: [[0,0],[0,0],[0,0],[0,0]],
+    prompt: "Where do you prefer to sit in a large rehearsal?",
+    weight: 0.1,
+    choices: ["no preference","near a friend","next to a stranger","in front of Robert"],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
 },
 {
-    prompt: "question5?",
-    choices: ["this","this2","that","that2"],
-    weight: [[0,0],[0,0],[0,0],[0,0]],
+    prompt: "Why can't you go to chariot?",
+    weight: 0.1,
+    choices: ["I don't like it.","I have to work/study/sleep.","I get wasted elsewhere","I'll always go."],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
+},
+{
+    prompt: "Holy shit, you run into Robert at a party. You",
+    weight: 0.1,
+    choices: ["panic.","ignore him.","make small talk.","offer him a drink."],
+    shift: [[0,0],[0,0],[0,0],[0,0]],
 },]
 
-var x = 0;
-var y = 0;
-var n = prompts.length;
-var answers = [];
+var x = 0.0;
+var y = 0.0;
+// var n = prompts.length;
+var questions = [];
 
 function shuffle(array) { //https://bost.ocks.org/mike/shuffle/
   var m = array.length, t, i;
@@ -40,29 +51,32 @@ function shuffle(array) { //https://bost.ocks.org/mike/shuffle/
   }
   return array;
 }
-//// uncomment to shuffle array
-// shuffle(prompts);
+// to shuffle array
+shuffle(prompts);
 
 function createPrompts() {
     // question prompts
     for (var i=0; i<prompts.length; i++) {
-        var prompt_li = document.createElement('li');
         var prompt_text = prompts[i].prompt;
+        var prompt_li = document.createElement('li');
         var prompt_textnode = document.createTextNode(prompt_text);
         prompt_li.className = 'question';
         prompt_li.appendChild(prompt_textnode);
         // answer choices
-        var choice = document.createElement('div');
-        choice.className = 'choice';
+        var question = document.createElement('div');
+        var question_id = 'number'+i;
+        question.setAttribute('id',question_id);
+        questions.push({id:question_id, answered:0});
         for (var j=0; j<prompts[i].choices.length; j++) {
-            var button_ = document.createElement('button');
             var button_text = prompts[i].choices[j];
-            var button_textnode = document.createTextNode(button_text);
+            var button_ = document.createElement('button');
             button_.className = 'btn';
+            button_.name = '' + j;
+            var button_textnode = document.createTextNode(button_text);
             button_.title = button_text;
             button_.appendChild(button_textnode);
-            choice.appendChild(button_);
-            prompt_li.appendChild(choice);
+            question.appendChild(button_);
+            prompt_li.appendChild(question);
         }
         // add to html
         document.getElementById('prompts').appendChild(prompt_li);
@@ -72,4 +86,20 @@ function createPrompts() {
 window.onload = function(){
     createPrompts();
     
+    $('.btn').mousedown (function() {
+        var id = $(this).closest('div').prop('id');
+        if($(this).hasClass('selected')) { // unselect
+            $(this).removeClass('selected');
+            questions.find(x => x.id===id).answered=0;
+        }
+        else { // unselect options
+            var buttons = document.getElementById(id).getElementsByTagName('button');
+            for (i=0; i<buttons.length; i++) {
+                buttons[i].classList.remove('selected');
+            }
+            // select
+            $(this).addClass('selected');
+            questions.find(x => x.id===id).answered=1;
+        }
+    })
 }
